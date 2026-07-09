5dd99e235078ef8d8f041fd1949f4cb90cf550c6
newtonfv <newtonfv@gmail.com>
2026-07-09 07:27:31 -0300

    Fix double timezone: manual -3h with UTC display

--- js/app.js
+++ js/app.js
@@ -114,7 +114,7 @@ function pipsHTML(n, small){
 function fmtDate(iso){
   const d = new Date(iso);
   d.setHours(d.getHours() - 3);
-  const opt = {day:'2-digit',month:'2-digit',year:'2-digit', hour:'2-digit',minute:'2-digit'};
+  const opt = {timeZone:'UTC', day:'2-digit',month:'2-digit',year:'2-digit', hour:'2-digit',minute:'2-digit'};
   return d.toLocaleString('pt-BR', opt);
 }
 function fmtDuration(sec){
